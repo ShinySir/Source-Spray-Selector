e7367f262c7aca7692be01a950565a43a86d8785
puro gaming <shinymegalucariods@gmail.com>
2026-03-12 22:52:51 +0800

    Update build.rs

--- build.rs
+++ build.rs
@@ -3,7 +3,10 @@ use winres;
 
 
 fn main() {
+	#[cfg(windows)]
+	{
     let mut res = winres::WindowsResource::new();
     res.set_icon("assets/icon.ico");
     res.compile().expect("Failed to compile Windows resources");
+	}
 }
